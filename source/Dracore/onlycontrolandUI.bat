b12edaa8340f68cae0f267b094975bc543de69de
TokyoHuskarl <froghuskarlsden@gmail.com>
2023-05-14 13:27:42 +0900

    マップロードシステムの組み換えと高低差エディット機能追加

---
+++ source/Dracore/onlycontrolandUI.bat
@@ -0,0 +1,23 @@
+@echo off
+
+rem // TPC exe
+set tpc="./../../tpc.exe"
+
+rem //log directory
+set log="./../../logs/log_"
+rem ==================================================================================
+
+set target=module_core_RTS_control_general
+echo Compiling RTS control. - %target%
+call %tpc% "./"%target%".tpc" > %log%%target%".txt" -en
+echo;
+
+
+set target=module_core_RTS_ui_general
+echo Compiling RTS ui. - %target%
+call %tpc% "./"%target%".tpc" > %log%%target%".txt" -en
+echo;
+
+
+echo All compile process finished. Es ist vorbei.
+pause
