df9017aa444bd13725a8a69dfacdfa91efe87bf9
TokyoHuskarl <froghuskarlsden@gmail.com>
2023-05-15 20:53:09 +0900

    Cohort処理軽量化

--- source/Dracore/onlybattle.bat
+++ source/Dracore/onlybattle.bat
@@ -12,6 +12,12 @@ echo Compiling RTS battle system. - %target%
 call %tpc% "./"%target%".tpc" > %log%%target%".txt" -en
 echo;
 
+set target=module_core_RTS_cohort_general
+echo Compiling cohort management system. - %target%
+call %tpc% "./"%target%".tpc" > %log%%target%".txt" -en
+echo;
+
+
 
 
 echo All compile process finished. Es ist vorbei.
