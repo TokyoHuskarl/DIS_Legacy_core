e2a7badcd733385b2cd0669402f60ce991547f09
TokyoHuskarl <froghuskarlsden@gmail.com>
2023-05-23 03:30:39 +0900

    UI support system setting 3

--- source/Dracore/onlycontrolandUI.bat
+++ source/Dracore/onlycontrolandUI.bat
@@ -18,6 +18,10 @@ echo Compiling RTS ui. - %target%
 call %tpc% "./"%target%".tpc" > %log%%target%".txt" -en
 echo;
 
+set target=module_core_Game_ui_general
+echo Compiling RTS ui. - %target%
+call %tpc% "./"%target%".tpc" > %log%%target%".txt" -en
+echo;
 
 echo All compile process finished. Es ist vorbei.
 pause
