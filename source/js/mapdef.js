5d19248f5b13510b7f6c081e6bfc091012f55ae9
TokyoHuskarl <froghuskarlsden@gmail.com>
2023-09-11 16:34:03 +0900

    adding script cmd

--- source/js/mapdef.js
+++ source/js/mapdef.js
@@ -1,7 +1,7 @@
 // EXAMPLE MAP DATA FOR PETROVKA
 RTS.map.defaultTileset = 15;
 RTS.map.size = [308,201];
-RTS.map.hasPresetHeightPic = true;
+RTS.map.heightgenType = HGEN_PRESET;
 
 
 deblog("Mapdef loaded")
