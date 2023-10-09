f4e336b6752561ef6f358f0b78c3ae9c483fab36
TokyoHuskarl <froghuskarlsden@gmail.com>
2023-10-09 20:51:38 +0900

    Faction GUI adjustment

---
+++ update_modules_dir.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Source directory to copy
+source_dir="./source/Modules"
+
+# Destination directory where you want to copy
+destination_dir="../"
+
+# Check if the source directory exists
+if [ -d "$source_dir" ]; then
+	# Create the destination directory if it doesn't exist
+	mkdir -p "$destination_dir"
+
+	# Copy the contents of the source directory to the destination directory
+	cp "$source_dir" "$destination_dir" -r 
+
+	echo "Directory copied successfully."
+else
+	echo "Source directory does not exist."
+fi
+
