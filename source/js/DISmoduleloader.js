44f40f417ac9b7213dc1d30ff9eb3b5469fb242e
TokyoHuskarl <froghuskarlsden@gmail.com>
2023-10-11 16:47:19 +0900

    def pid_asd

--- source/js/DISmoduleloader.js
+++ source/js/DISmoduleloader.js
@@ -1,14 +1,29 @@
-// DIS module loader
+// DIS moduleinfo loader
 
 
 // parse t[520] <- Str_moduleinfo
-const moduleinfo = JSON.parse(gett(520));
+let moduleinfo;
 
-moduleinfo.getModuleInfoForTitle = function(){
-	
-	return (this.name + " " + this.version )
+try {
+	moduleinfo = JSON.parse(gett(520));
+	moduleinfo.getModuleInfoForTitle = function(){
+		
+		return (this.name + " " + this.version )
 
+	};
+
+} catch(error) {
+	moduleinfo = {
+		name: "undefined",
+		version: "undefined",
+		getModuleInfoForTitle(){
+		
+		return ("\\c[17]ERROR: moduleinfo.json is missing!")
+
+		},
+	};
 };
+
 
 // return t[520] to current version
 sett(520,moduleinfo.getModuleInfoForTitle());
